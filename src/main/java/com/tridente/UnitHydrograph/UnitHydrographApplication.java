d79871938a0af88cd10780aaaad0d2f0f32fd2f3
AlejandroNahuel <alejandro.nahuel15817@gmail.com>
2022-06-14 10:39:44 -0300

    fourth commit

---
+++ src/main/java/com/tridente/UnitHydrograph/UnitHydrographApplication.java
@@ -0,0 +1,13 @@
+package com.tridente.UnitHydrograph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitHydrographApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitHydrographApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tridente/UnitHydrograph/UnitHydrographApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tridente.UnitHydrograph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitHydrographApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
